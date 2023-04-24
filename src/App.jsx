6d278618677c29b281d41fdefbf4ce44ff633d0b
AJ <joysondsouzaaj@gmail.com>
2023-04-24 12:07:34 +0530

    Navbar componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
  
   return (
     <>
-       <h1>
-        Hello woorld
-       </h1>
+       <Navbar />
     </>
   )
 }
